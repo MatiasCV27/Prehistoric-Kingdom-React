1c32012ad766819a3a9786959a7edf9b4dc150aa
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-20 12:18:08 -0500

    Mostrar la info terminada

--- src/Componentes/PeriodoJurasico.js
+++ src/Componentes/PeriodoJurasico.js
@@ -17,25 +17,55 @@ const PeriodoJurasico = () => {
 
     return (
         <div className='App'>
-            <div className="container-fluid">
+            <div className="container">
                 <div className='row mt-3'>
                 {
                   especies
                     .filter(especie => especie.Periodo === 'Jurásico') 
                     .map((especie, i) => (
-                    <tr key={especie.IdEspecie}>
-                        <td>{i + 1}</td>
-                        <td>{especie.Nombre}</td>
-                        <td>{especie.Significado}</td>
-                        <td>{especie.Dieta}</td>
-                        <td>{especie.Peso}</td>
-                        <td>{especie.Periodo}</td>
-                        <td>{especie.Hallazgo}</td>
-                        <td>{especie.Dimensiones}</td>
-                        <td>{especie.Descripcion}</td>
-                        <td>{especie.Tiempo}</td>
-                        <td><img src={especie.Imagen} width='50px' alt={`Imagen de ${especie.Nombre}`} /></td>
-                    </tr>
+                    <div key={especie.IdEspecie} className='col-lg-4 col-md-6 col-sm-12 mx-auto mb-5'>
+                        <div class="cardMostrar">
+                          <div class="content">
+                            <div class="back">
+                              <div class="back-content">
+                                <img src={especie.Imagen} className='imgPEspecie'/>
+                              </div>
+                            </div>
+                            <div class="front">
+                              <div class="img">
+                                <div class="circle">
+                                </div>
+                                <div class="circle" id="right">
+                                </div>
+                                <div class="circle" id="bottom">
+                                </div>
+                              </div>
+                              <div class="front-content contEspecie">
+                                <div class="description">
+                                  <div class="">
+                                    <p class="text-center">
+                                      <strong>{especie.Nombre}</strong> <br></br>
+                                      <i>{especie.Significado}</i>
+                                    </p>
+                                  </div>
+                                  <div class="textoCard"><strong>Descripción: </strong><p>{especie.Descripcion}</p></div>
+                                  <div className='row'>
+                                    <div class="textoCard col-6 text-center"><strong>Dieta: </strong><p>{especie.Dieta}</p></div>
+                                    <div class="textoCard col-6 text-center"><strong>Hallazgo: </strong><p>{especie.Hallazgo}</p></div>
+                                  </div>
+                                  <div className='row'>
+                                    <div class="textoCard col-6 text-center"><strong>Dimensiones: </strong><p>{especie.Dimensiones}</p></div>
+                                    <div class="textoCard col-6 text-center"><strong>Tiempo: </strong><p>{especie.Tiempo}</p></div>
+                                  </div>
+                                  <div className='row mx-auto'>
+                                    <div class="textoCard col-12 text-center"><strong>peso: </strong><p>{especie.Peso}</p></div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div> 
                     ))
                 }
                 </div>
