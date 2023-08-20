4a3f83a4d1490460934a173f0fe7fada1a661f44
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-20 14:22:16 -0500

    Agregando el Contacto

--- src/Prehistoric-Routes/Contacto.jsx
+++ src/Prehistoric-Routes/Contacto.jsx
@@ -4,9 +4,45 @@ export default function Contacto() {
     return (
         <div>
             <Navbar_Footer />
-            <div className="pt-5 mt-5">
-                <h1>Aqui si hay tiempo se hara el contacto</h1>
+            <div className="pt-5 mt-5"></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8">
+                        <div className="imgContacto">
+                            <div className="overlayContacto"></div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="login-box">
+                          <h4 className="text-white text-center" style={{ padding: ".75em 0" }}>
+                            <u>Contactanos para más información</u>
+                          </h4>
+                          <form>
+                            <div className="user-box">
+                              <input type="text" name="user" required="" />
+                              <label>Nombre Completo:</label>
+                            </div>
+                            <div className="user-box">
+                              <input type="text" name="" required=""/>
+                              <label>Correo Electrónico:</label>
+                            </div>
+                            <div className="user-box">
+                              <input type="text" name="" required=""/>
+                              <label>Teléfono:</label>
+                            </div>
+                            <div className="user-box">
+                              <input type="text" name="" required=""/>
+                              <label>Consulta:</label>
+                            </div>
+                            <center>
+                              <a id="EnviarB"><strong>Enviar</strong><span></span></a>
+                            </center>
+                          </form>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div className="pt-5 mt-5"></div>
         </div>
     );    
 }
